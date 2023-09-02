d2c140266efbf6dec98db1456a9cbc713fd85c26
Andrew <asoborev@mail>
2023-09-02 15:11:17 +0300

    создал кастомный хук для проверки авторизации

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,18 @@
-import { useSelector, useDispatch } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import type { RootState, AppDispatch } from './store';
+import type { AppDispatch, RootState } from './store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useAuth = () => {
+    const { email, token, id } = useAppSelector((state) => state.user);
+
+    return {
+        isAuth: !!email,
+        email,
+        token,
+        id,
+    };
+};
