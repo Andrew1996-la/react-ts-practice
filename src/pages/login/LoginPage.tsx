320f867ae90c7c35e5907d68b611eb6ee8112ba0
Andrew <asoborev@mail>
2023-09-03 22:46:08 +0300

    запись из login формы

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -3,15 +3,33 @@ import { Link } from 'react-router-dom';
 import s from './loginPage.module.css';
 import { ButtonStandart } from '../../ui/buttons/Button';
 import { InputStandart, InputPassword } from '../../ui/inputs/input';
-
-
+import { useState } from 'react';
 
 const LoginPage = () => {
+    
+    const [login, setLogin] = useState<string | null>(null);
+    const [password, setPassword] = useState<string | null>(null);
+
+    const handleUserInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const typeInput = e.target.getAttribute('data-type');
+        typeInput === 'login' ? setLogin(e.target.value) : setPassword(e.target.value);
+    };
+
     return (
         <form className={s.loginForm}>
             <h2 className={s.greeting}>Добро пожаловть!</h2>
-            <InputStandart type='text' placeholder='введите логин' />
-            <InputPassword type='password' placeholder='введите пароль' />
+            <InputStandart
+                handleUserInfo={(e) => handleUserInfo(e)}
+                type='text'
+                placeholder='введите логин'
+                dataAttribute='login'
+            />
+            <InputPassword
+                handleUserInfo={(e) => handleUserInfo(e)}
+                type='password'
+                placeholder='введите пароль'
+                dataAttribute='password'
+            />
             <div className={s.btnPanel}>
                 <Link className={s.link} to='/'>
                     <ButtonStandart>Login</ButtonStandart>
